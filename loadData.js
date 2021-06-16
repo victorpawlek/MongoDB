b8e20cac7617ee8a456942e0ab2f8a4e85fd8b6e
victorpawlek <pawlek.v03@htlwienwest.at>
2021-06-16 12:10:26 +0200

    fertig

--- loadData.js
+++ loadData.js
@@ -1,18 +1,20 @@
 require('./db/connect');
 require('colors');
+const dogs=require('./dogs.json')
 const { Dog } = require('./model/schemas');
 
 (async () => {
   try {
-    const p1 = Dog.create({
-      name: 'Bello',
-      born: '2020-12-01',
-    });
-    const p2 = Dog.create({
-      name: 'Beisser',
-      born: '2020-12-02',
-    });
-    await Promise.all([p1, p2]);
+    // const p1 = Dog.create({
+    //   name: 'Bello',
+    //   born: '2020-12-01',
+    // });
+    // const p2 = Dog.create({
+    //   name: 'Beisser',
+    //   born: '2020-12-02',
+    // });
+    // await Promise.all([p1, p2]);
+    await Dog.insertMany(dogs);
     console.log('Data loaded');
   } catch (error) {
     console.error(`Error ====> ${error.message.red}`);
--- removeData.js
+++ removeData.js
@@ -4,10 +4,12 @@ const { Dog } = require('./model/schemas');
 
 (async () => {
     try {
-        await Dog.deleteMany();
-        console.log('Data deleted');
+      await Dog.deleteMany();
+      console.log('Data deleted');
     } catch (error) {
-        console.error(`Error ====> ${error.message.red}`);
+      console.error(`Error ====> ${error.message.red}`);
+    } finally {
+      process.emit('SIGINT');
     }
 })();
     
